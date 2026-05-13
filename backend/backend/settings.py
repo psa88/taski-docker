166f9f6f2fc4a7ea5ad7132341c6528d6c487a45
Sergey Pryadko <sergeypryadko@gmail.com>
2026-05-13 18:50:55 +0300

    new dns

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['testik.hopto.org', '81.26.186.32', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['taskipro.hopto.org', '81.26.186.32', 'localhost', '127.0.0.1']
 
 
 # Application definition
